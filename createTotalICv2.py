154657fe214a6b5262ef39c8ac1636c2bc87fffc
joel nipper <joel.nipper@charlottenc.gov>
2017-07-31 15:18:32 -0400

    Script to create total ic layer, improved
    Dropped edge of pavement, retained subtheme and jurisdiction fields

---
+++ createTotalICv2.py
@@ -0,0 +1,45 @@
+#### File to take cross sections, make maps
+
+## Import modules
+import arcpy
+import csv
+import os
+import shutil
+
+## Set workspace
+arcpy.env.workspace = r'C:\Users\95218\Documents\ArcGIS\SCM Crediting'
+
+## Create Total IC Layer
+# Set local variables
+xy_tol = ""
+
+## IC Layers from X:\
+ic_comm = "X:\stormdat\public\impervious_surface\Commercial_Impervious.shp"
+ic_sf = "X:\stormdat\public\impervious_surface\Singlefamily_Impervious.shp"
+ic_other = "X:\stormdat\public\impervious_surface\Impervious_Surface_Other.shp"
+
+## Merge them to...
+out_feat = "TotalIC_31July2017.shp"
+
+## Need to do this field mapping thing to get around a write error, suspected
+## related to non matching fields in the IC layers to merged. To-do: learn pyth
+fieldMappings = arcpy.FieldMappings()
+fieldMappings.addTable(ic_comm)
+fieldMappings.addTable(ic_sf)
+fieldMappings.addTable(ic_other)
+
+for field in fieldMappings.fields:
+    if field.name not in ["impervious", "st_area_sh", "subtheme", "Shape_STAr", "Subtheme", "jurisdicti"]:
+        fieldMappings.removeFieldMap(fieldMappings.findFieldMapIndex(field.name))
+
+arcpy.Merge_management([ic_comm, ic_sf, ic_other], out_feat, fieldMappings)
+
+######################### Geometry ##########################################
+arcpy.AddField_management("TotalIC_31July2017.shp", "areaSF", "FLOAT", 16, 1, "",
+                          "temp", "NULLABLE", "REQUIRED")
+
+arcpy.CalculateField_management("TotalIC_31July2017.shp", "areaSF",
+                                "!shape.area@squarefeet!", "PYTHON_9.3", "")
+
+
+
